e1eb24e32b56e84ac8543b088bc65926a5491d12
softprops <d.tangren@gmail.com>
2019-01-10 01:00:11 -0500

    simplify

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -1,13 +1,14 @@
 use failure::Fail;
 use imap::error::Error as IMapError;
 use mailparse::MailParseError;
-use native_tls::TlsStream;
+use native_tls::{Error as TlsError, TlsStream};
 use std::{error::Error as StdError, fmt, net::TcpStream};
 
 #[derive(Fail, Debug)]
 pub enum Error {
     IMap(IMapError),
     Parse(MailParseError),
+    Tls(TlsError),
 }
 
 impl fmt::Display for Error {
@@ -18,6 +19,7 @@ impl fmt::Display for Error {
         match self {
             Error::IMap(e) => write!(f, "{}", e.description()),
             Error::Parse(e) => write!(f, "{}", e.description()),
+            Error::Tls(e) => write!(f, "{}", e.description()),
         }
     }
 }
@@ -38,5 +40,11 @@ impl From<IMapError> for Error {
 impl From<MailParseError> for Error {
     fn from(e: MailParseError) -> Self {
         Error::Parse(e)
+    }
+}
+
+impl From<TlsError> for Error {
+    fn from(e: TlsError) -> Self {
+        Error::Tls(e)
     }
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -38,40 +38,34 @@ pub struct Part {
 
 impl From<ParsedMail<'_>> for Message {
     fn from(parsed: ParsedMail) -> Self {
-        let body = if parsed.ctype.mimetype.starts_with("multipart/") {
-            parsed
-                .subparts
-                .into_iter()
-                .fold(Vec::new(), |mut result, part| {
-                    let body = if part.ctype.mimetype.starts_with("text/") {
-                        part.get_body().ok()
-                    } else {
-                        part.get_body_raw().ok().map(|b| base64::encode(&b))
-                    };
-                    result.push(Part {
-                        content_type: part.ctype.mimetype,
-                        body,
-                    });
-                    result
-                })
-        } else {
-            let body = if parsed.ctype.mimetype.starts_with("text/") {
-                parsed.get_body().ok()
-            } else {
-                parsed.get_body_raw().ok().map(|b| base64::encode(&b))
-            };
-            vec![Part {
-                content_type: parsed.ctype.mimetype,
-                body,
-            }]
-        };
-        let headers = parsed
-            .headers
+        let headers = (&parsed.headers)
             .into_iter()
             .fold(HashMap::new(), |mut result, header| {
-                result.insert(header.get_key().unwrap(), header.get_value().unwrap());
+                result.insert(
+                    header.get_key().unwrap_or_default(),
+                    header.get_value().unwrap_or_default(),
+                );
                 result
             });
+
+        let parts = if parsed.ctype.mimetype.starts_with("multipart/") {
+            parsed.subparts
+        } else {
+            vec![parsed]
+        };
+        let body = parts.into_iter().fold(Vec::new(), |mut result, part| {
+            let body = if part.ctype.mimetype.starts_with("text/") {
+                part.get_body().ok()
+            } else {
+                part.get_body_raw().ok().map(|b| base64::encode(&b))
+            };
+            result.push(Part {
+                content_type: part.ctype.mimetype,
+                body,
+            });
+            result
+        });
+
         Message {
             headers: headers.clone(),
             subject: headers.get("Subject").cloned(),
@@ -132,7 +126,7 @@ impl Client {
     where
         M: AsRef<str>,
     {
-        let tls = TlsConnector::builder().build().unwrap();
+        let tls = TlsConnector::builder().build()?;
         let client = imap::connect((self.domain.as_str(), self.port), &self.domain, &tls)?;
         let mut session = client.login(&self.username, &self.password)?;
         session.select(mailbox.as_ref())?;
